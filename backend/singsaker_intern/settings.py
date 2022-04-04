cdc64eefe78a94cb15b2de714e38fb141750dbb2
Mathias Raa <skraagen@hotmail.com>
2022-04-05 00:27:43 +0200

    awd

--- backend/singsaker_intern/settings.py
+++ backend/singsaker_intern/settings.py
@@ -21,7 +21,7 @@ env = environ.Env(
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
