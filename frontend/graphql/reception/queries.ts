0277f391082ebd3a705e0eaaf62075a328515835
Mathias Raa <skraagen@hotmail.com>
2022-04-04 17:16:42 +0200

    add missing mutations and queries

--- frontend/graphql/reception/queries.ts
+++ frontend/graphql/reception/queries.ts
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
 export const GET_SHIFT_DATES = gql`
-  query allShiftDates($semester: ID!) {
-    allShiftDates(semester: $semester) {
+  query allShiftDates($semester: ID!, $member: ID) {
+    allShiftDates(semester: $semester, member: $member) {
       date
       shifts {
         id
