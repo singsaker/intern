5a62a9473c6085771033101a62a5c04692c994c8
Mathias Raa <skraagen@hotmail.com>
2022-04-04 17:16:30 +0200

    clean up backend

--- backend/shops/schema.py
+++ backend/shops/schema.py
@@ -1,8 +1,9 @@
-from .types import ShopType
-from .resolvers import ShopResolvers
 import graphene
+
+from .resolvers import ShopResolvers
+from .types import ShopType
 
 
 class ShopQueries(graphene.ObjectType, ShopResolvers):
     all_shops = graphene.List(ShopType)
-    shop = graphene.Field(ShopType, id=graphene.ID(required=True))
+    shop = graphene.Field(ShopType, slug=graphene.String(required=True))
