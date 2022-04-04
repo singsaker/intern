f8bdfa6f565b16566ff22a252872751d2d76a46a
Mathias Raa <skraagen@hotmail.com>
2022-04-04 20:47:36 +0200

    add postgres to production shell file

--- backend/entrypoints/production.sh
+++ backend/entrypoints/production.sh
@@ -3,5 +3,5 @@ set -e
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
+./wait-for-it.sh postgres:5432 -- gunicorn config.wsgi:application --bind 0.0.0.0:8000
 exec "$@"
