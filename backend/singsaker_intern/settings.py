2282eeeca63c819c23849abd45dfb8c94412a5f8
Mathias Raa <skraagen@hotmail.com>
2022-04-04 23:17:18 +0200

    another path update

--- backend/singsaker_intern/settings.py
+++ backend/singsaker_intern/settings.py
@@ -22,7 +22,7 @@ env = environ.Env(
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
@@ -162,7 +162,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = str(ROOT_DIR / "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
