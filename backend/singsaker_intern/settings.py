63fae5de6c5c6feed7231631c580d04eabf6cd0d
Mathias Raa <skraagen@hotmail.com>
2022-04-05 01:00:29 +0200

    awd

--- backend/singsaker_intern/settings.py
+++ backend/singsaker_intern/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
