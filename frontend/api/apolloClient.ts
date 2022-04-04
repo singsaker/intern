f42e56e9e2967541e3c6b81192bb81b12121954e
Mathias Raa <skraagen@hotmail.com>
2022-04-04 21:36:57 +0200

    make them talk together

--- frontend/api/apolloClient.ts
+++ frontend/api/apolloClient.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 
+const uri = typeof window === "undefined" ? "http://127.0.0.1:8000/graphql/" : "http://genfors.singsaker.no:8000/graphql/";
+
 const link = createHttpLink({
-  uri: "http://127.0.0.1:8000/graphql/",
+  uri: uri,
   credentials: "include",
 });
 
