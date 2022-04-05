d55c47a03937f68f2a906023428ab4dfcf00baac
Mathias Raa <skraagen@hotmail.com>
2022-04-05 22:30:03 +0200

    update django production urls conf

--- backend/singsaker_intern/settings/production.py
+++ backend/singsaker_intern/settings/production.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["genfors.singsaker.no"
 CORS_ORIGIN_WHITELIST = env("CORS_ORIGIN_WHITELIST")
 
 # URLs
-ROOT_URLCONF = "config.urls"
+ROOT_URLCONF = "singsaker_intern.urls"
 
 # GRAPHENE
 GRAPHQL_JWT = {"JWT_COOKIE_DOMAIN": env("JWT_COOKIE_DOMAIN", default="genfors.singsaker.no")}
