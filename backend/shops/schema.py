2c96276086a2de5c21d7a4ef27acec1dd1abbc56
Mathias Raa <skraagen@hotmail.com>
2022-04-10 18:53:45 +0200

    implement sales in backend

--- backend/shops/schema.py
+++ backend/shops/schema.py
@@ -1,9 +1,29 @@
 import graphene
 
+from .mutations import (
+    CreateProduct,
+    CreateProductCategory,
+    CreateSale,
+    DeleteProduct,
+    DeleteProductCategory,
+    UpdateProduct,
+    UpdateProductCategory,
+)
 from .resolvers import ShopResolvers
-from .types import ShopType
+from .types import SaleType, ShopType
 
 
 class ShopQueries(graphene.ObjectType, ShopResolvers):
     all_shops = graphene.List(ShopType)
+    all_sales = graphene.List(SaleType, shop=graphene.ID(required=True), member=graphene.ID(required=False))
     shop = graphene.Field(ShopType, slug=graphene.String(required=True))
+
+
+class ShopMutations(graphene.ObjectType):
+    create_product_category = CreateProductCategory.Field()
+    update_product_category = UpdateProductCategory.Field()
+    delete_product_category = DeleteProductCategory.Field()
+    create_product = CreateProduct.Field()
+    update_product = UpdateProduct.Field()
+    delete_product = DeleteProduct.Field()
+    create_sale = CreateSale.Field()
--- backend/shops/types.py
+++ backend/shops/types.py
@@ -1,7 +1,23 @@
 from graphene_django import DjangoObjectType
-from .models import Shop
+
+from .models import Product, ProductCategory, Sale, Shop
 
 
 class ShopType(DjangoObjectType):
     class Meta:
         model = Shop
+
+
+class ProductCategoryType(DjangoObjectType):
+    class Meta:
+        model = ProductCategory
+
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+
+
+class SaleType(DjangoObjectType):
+    class Meta:
+        model = Sale
